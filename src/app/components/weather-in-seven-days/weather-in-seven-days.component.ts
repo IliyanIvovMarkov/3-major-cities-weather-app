484958beb078c0043f64ea375568439d1db1baab
IliyanIvovMarkov <i.markov@allfront.io>
2022-01-11 13:45:39 +0200

    Add routing and clean up scss

---
+++ src/app/components/weather-in-seven-days/weather-in-seven-days.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-in-seven-days',
+  templateUrl: './weather-in-seven-days.component.html',
+  styleUrls: ['./weather-in-seven-days.component.scss']
+})
+export class WeatherInSevenDaysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
