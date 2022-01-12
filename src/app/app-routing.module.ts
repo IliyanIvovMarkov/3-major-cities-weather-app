de3e721f8e834ae03e0310f0947167f1c585418e
IliyanIvovMarkov <i.markov@allfront.io>
2022-01-12 18:23:13 +0200

    Add inputs to components and conditional styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {WeatherInSevenDaysComponent} from "./components/weather-in-seven-days/weather-in-seven-days.component";
 import {DetailsPageComponent} from "./components/details-page/details-page.component";
+import {WeatherTodayComponent} from "./components/weather-today/weather-today.component";
 
 const routes: Routes = [
   { path: 'seven-days/:city', component: WeatherInSevenDaysComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { DetailsPageComponent } from './components/details-page/details-page.component';
 import { SearchComponent } from './components/search/search.component';
 import { CityWeatherInfoComponent } from './components/city-weather-info/city-weather-info.component';
+import { WeatherTodayComponent } from './components/weather-today/weather-today.component';
+import {DayForecastComponent} from "./components/day-forecast/day-forecast.component";
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { CityWeatherInfoComponent } from './components/city-weather-info/city-we
     DetailsPageComponent,
     SearchComponent,
     CityWeatherInfoComponent,
+    WeatherTodayComponent,
+    DayForecastComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
